203c7a61f004c3da34de3707d1908079db2ba2f3
Enzo Cassemiro <enzocass@live.com>
2020-08-02 19:54:42 -0300

    Run build

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
 import Home from './pages/Home';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-
-const Erro404 = () =>(<div>404-Not Found</div>)
+const Erro404 = () => (<div>404-Not Found</div>);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} exact/> 
-      <Route path="/cadastro/video" component={CadastroVideo} /> 
-      <Route path="/cadastro/categoria" component={CadastroCategoria} /> 
-      <Route component={Erro404} /> 
-    </Switch>  
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Erro404} />
+    </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
